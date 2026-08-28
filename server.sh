d9753868f55efee914da78df64effcd42cd570df
𝕾𝖙𝖆𝖆𝖙𝖘𝖗𝖆𝖙 <staatsrat@gmx.net>
2026-08-28 10:03:42 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,12 +1,19 @@
-x=`sudo timeout 5s tcpdump -l | grep --line-buffered echo | grep --line-buffered request | stdbuf -oL cut -d'h' -f2- | stdbuf -oL cut -d'h' -f2- | stdbuf -oL tr -d ' '::`
+#!/bin/bash
+sudo tcpdump -l -n -i any icmp and icmp[icmptype] == icmp-echo 2>/dev/null | awk '
+match($0, /length ([0-9]+)/, a) {
+    l = a[1]; if (l == 64) next;
+    if (l >= 32 && l <= 126) {
+        c = sprintf("%c", l);
+        if (c != "*") m = m c;
+        else {
+            print m;
+            if (substr(m, length(m)-1) == "+x") {
+                msg_clean = substr(m, 1, length(m)-2);
+                #Command execution if +x Be aware!
+	            	#system(msg_clean);
+            }
+            m = "";
+        }
+    }
+}'
 
-echo "Decimal: " $x
-
-echo "$(printf '\\%03o' $x)"; echo
-
-y="$(printf '\\%03o' $x)"; echo
-
-#This function will execute every send command! 
-#Only use if needed!
-
-# echo "$y" | bash 
