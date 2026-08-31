3389498a0a2b4f9c9fcbe3f2495ff237abe2fab8
𝕾𝖙𝖆𝖆𝖙𝖘𝖗𝖆𝖙 <staatsrat@gmx.net>
2026-08-31 21:17:05 +0200

    Enhance server.sh for better ICMP packet handling
    Refactor tcpdump command and improve IP and length matching logic.

--- server/server.sh
+++ server/server.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-sudo tcpdump -l -n -i any icmp and icmp[icmptype] == icmp-echo 2>/dev/null | awk '
-match($0, /length ([0-9]+)/, a) {
-    l = a[1]; if (l == 64) next;
+sudo tcpdump -l -n -i any icmp and 'icmp[icmptype] == icmp-echo' 2>/dev/null | awk '
+match($0, /IP ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/, ip_match) && match($0, /length ([0-9]+)/, len_match) {
+    src_ip = ip_match[1]
+    l = len_match[1]
+    
+    if (l == 64) next
+    
     if (l >= 32 && l <= 126) {
-        c = sprintf("%c", l);
-        if (c != "*") m = m c;
-        else {
-            print m;
+        c = sprintf("%c", l)
+        if (c != "*") {
+            m = m c
+        } else {
+            cmd = sprintf("sudo ./send %s %s", m, src_ip)
+            system(cmd)
+            
             if (substr(m, length(m)-1) == "+x") {
-                msg_clean = substr(m, 1, length(m)-2);
-                #Command execution if +x Be aware!
-	            #system(msg_clean);
+                msg_clean = substr(m, 1, length(m)-2)
             }
-            m = "";
+            m = ""
         }
     }
-}'
-
-
